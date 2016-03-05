b033af8d2cac896a06ab185db4f7f63f07de85d3
Giu Nator <mspwelt44@gmail.com>
2016-03-05 11:01:17 +0100

    Update and rename README.md to fakevirushahahaomg.bat

---
+++ fakevirushahahaomg.bat
@@ -0,0 +1,8 @@
+@echo off
+pause
+color A
+mode 1000
+
+:matrix
+echo %random%%random%%random%%random%%random%­­%random%%random%%random%%random%%random­%­%random%%random%%random%%random%%rando­m%­%random%%random%%random%%random%%rand­om%­%random%%random%
+goto matrix
